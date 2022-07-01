ff5317634c859eb1f25fe52e98be66294225fd08
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2022-07-01 01:49:32 -0300

    Add files via upload

---
+++ Nodeshop/src/Footer.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+class Footer extends React.Component{
+    render() {
+        return (
+
+            <footer className='rodape'>
+                <p>Loja do Obama desenvolido por: <span>Amanda Vitoria Valentim dos anjos</span> e <span>João Pedro Gonçalves da Silva</span> - Todos os direitos reservados </p>
+            </footer>
+
+        );
+    }
+}
+
+export default Footer;
